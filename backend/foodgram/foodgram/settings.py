176e176c400197d3e4147db25f8fc49179ce8c83
Max Stepanov <frermax@yandex.ru>
2022-11-05 16:50:54 +0600

    project

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'j2wwwf(l&_h=%4+a1vv5zt%y!vut7_8)uz+tcvp=j5wq29s@*m'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '158.160.7.66',
+    '158.160.3.253',
     'host.docker.internal',
     'localhost',
 ]
